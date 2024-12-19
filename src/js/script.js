da5ede47051c7268443e1032aa9aab2c31fda628
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-12-19 17:58:08 +0300

    menu html + css + jave

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+// Menu
+
+const menuToggle = document.querySelector('#button-header');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
+  menuToggle.setAttribute('aria-expanded', !isExpanded);
+
+  menu.classList.toggle('open');
+
+  if (!isExpanded) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
